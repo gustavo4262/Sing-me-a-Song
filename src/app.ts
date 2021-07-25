ad291ae9672fd4ad2cd7bea65947fc67df64b07f
cyclopz66 <gustavomartinspinheiro@gmail.com>
2021-07-25 13:30:12 -0300

    initial configurations and database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
+import connection from "./database";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  res.send("OK!");
-});
+app.get('/recommendations', async(req, res) => {
+  
+  try{
+
+    const result = await connection.query('select * from recommendations')
+    
+    const suer = result.rows
+    return res.send(suer)
+  }
+  catch(err) {
+    console.log(err.message)
+    return res.sendStatus(500)
+  }
+} );
 
 export default app;
